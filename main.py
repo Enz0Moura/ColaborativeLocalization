5a70a8c25eab798b3185d900ff8244e100d53bd8
Enzo Moura <12929052716@cefet-rj.br>
2024-03-26 06:03:39 -0300

    General fixes.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from bracelet.models import Terminal as TerminalModel
+from terminal.models import Terminal as TerminalModel
 from totem.models import Totem as TotemModel
 from server.models import Server as ServerModel
-from sim.strategies import send_bracelet_beacons
+from sim.strategies import send_terminal_beacons
 
 bracelet = TerminalModel()
 totem = TotemModel()
 server = ServerModel()
 
 
-send_bracelet_beacons(bracelet.send_data(), totem)
+send_terminal_beacons(bracelet.send_data(), totem)
 
 server.receive_data(totem.send_memory())
 print(bracelet.memory)
